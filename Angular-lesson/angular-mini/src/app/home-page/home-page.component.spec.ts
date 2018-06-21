3873477812244413643e1ded356f4b86e8232de6
romprokop <romekprokop@gmail.com>
2018-06-21 17:45:02 +0300

    angular lessons

---
+++ Angular-lesson/angular-mini/src/app/home-page/home-page.component.spec.ts
@@ -0,0 +1,3 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomePageComponent } from './home-page.component';
---
+++ Angular-lesson/angular-mini/src/app/setup-page/setup-page.component.spec.ts
@@ -0,0 +1,3 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SetupPageComponent } from './setup-page.component';
